959e4315a9a10328db22fb35bd6ebe662baf72d6
manindervir <308276@canadorestudents.ca>
2022-11-02 08:07:22 -0400

    added initial comit for

---
+++ app/src/main/java/com/example/starvingsnake/GameLevelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.starvingsnake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameLevelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_level)
+    }
+}
