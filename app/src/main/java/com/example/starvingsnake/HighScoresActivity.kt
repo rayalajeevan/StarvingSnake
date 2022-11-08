d4085dd3d9411df5e6066601511741f7d97049e3
Jeevan <jrayala@evoketechnologies.com>
2022-11-08 00:56:11 -0500

    added animations

--- app/src/main/java/com/example/starvingsnake/HighScoresActivity.kt
+++ app/src/main/java/com/example/starvingsnake/HighScoresActivity.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.os.Handler
+import android.widget.*
+import com.google.android.material.animation.AnimationUtils
 
 class HighScoresActivity : AppCompatActivity() {
-    @SuppressLint("Range")
+    @SuppressLint("Range", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_high_scores)
         val home_btn=findViewById<ImageView>(R.id.imageView3)
+        val highscore_text=findViewById<TextView>(R.id.textViewhigh)
+        val frame_btn=findViewById<RelativeLayout>(R.id.highscores_frame)
         var player_1_username=findViewById<TextView>(R.id.player_1_username)
         var player_2_username=findViewById<TextView>(R.id.player_2_username)
         var player_3_username=findViewById<TextView>(R.id.player_3_username)
@@ -24,7 +27,7 @@ class HighScoresActivity : AppCompatActivity() {
         if (cursor?.moveToFirst()==true) {
             player_1_username.setText(cursor?.getString(cursor.getColumnIndex(DatabaseAdapter.HIGHSCORE_USER_NAME)))
             player_1_score.setText(
-                "Score is :" + cursor?.getInt(
+                 cursor?.getInt(
                     cursor?.getColumnIndex(
                         DatabaseAdapter.HIGHSCORE
                     )
@@ -34,7 +37,7 @@ class HighScoresActivity : AppCompatActivity() {
             if (cursor?.moveToNext()==true) {
             player_2_username.setText(cursor?.getString(cursor.getColumnIndex(DatabaseAdapter.HIGHSCORE_USER_NAME)))
             player_2_score.setText(
-                "Score is :" + cursor?.getInt(
+                 cursor?.getInt(
                     cursor?.getColumnIndex(
                         DatabaseAdapter.HIGHSCORE
                     )
@@ -44,7 +47,7 @@ class HighScoresActivity : AppCompatActivity() {
         if (cursor?.moveToNext()==true) {
             player_3_username.setText(cursor?.getString(cursor.getColumnIndex(DatabaseAdapter.HIGHSCORE_USER_NAME)))
             player_3_score.setText(
-                "Score is :" + cursor?.getInt(
+                 cursor?.getInt(
                     cursor?.getColumnIndex(
                         DatabaseAdapter.HIGHSCORE
                     )
@@ -55,6 +58,15 @@ class HighScoresActivity : AppCompatActivity() {
             val intent=Intent(this,HomePageActivity::class.java)
             startActivity(intent)
         }
+        val ani_slide=android.view.animation.AnimationUtils.loadAnimation(applicationContext,R.animator.slide)
+        val ani_slide_up=android.view.animation.AnimationUtils.loadAnimation(applicationContext,R.animator.slide_up)
+        frame_btn.startAnimation(ani_slide_up)
+        home_btn.startAnimation(ani_slide)
+        highscore_text.startAnimation(ani_slide)
+        player_1_username.startAnimation(ani_slide_up)
+        player_1_score.startAnimation(ani_slide_up)
+        player_2_username.startAnimation(ani_slide_up)
+        player_2_score.startAnimation(ani_slide_up)
 
     }
 }
